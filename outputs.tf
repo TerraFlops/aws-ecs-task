5d7d882e7cbc53e9fd23ee5c255d9f1a08352948
Damian Sloane <damian@capacitorlabs.com>
2020-08-26 16:06:19 +1000

    Work in progress

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,15 @@ output "iam_role_arn" {
 output "iam_role_name" {
   value = module.task_iam_role.iam_role_name
 }
+
+output "ecs_cluster_name" {
+  value = var.cluster_name
+}
+
+output "ecs_service_id" {
+  value = aws_ecs_service.task.id
+}
+
+output "ecs_service_name" {
+  value = aws_ecs_service.task.name
+}
