bdd82e08d644bd046e76078dd281c8ba50c0b359
Damian Sloane <damian@capacitorlabs.com>
2020-10-09 18:05:26 +1100

    Work in progress

--- outputs.tf
+++ outputs.tf
@@ -48,6 +48,11 @@ output "alb_arn" {
   value = var.alb_enabled == true ? module.task_alb[0].alb_arn : null
 }
 
+output "alb_dns_name" {
+  description = "If an ALB was created this will contain the DNS name"
+  value = var.alb_enabled == true ? module.task_alb[0].alb_dns_name : null
+}
+
 output "alb_certificate_arn" {
   description = "If an ALB was created this will contain the ARN of certificate linked to it"
   value = local.alb_certificate_arn
