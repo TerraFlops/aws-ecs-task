8336e9b49050e885cc39277f1df7480efadce360
Damian Sloane <damian@capacitorlabs.com>
2020-11-09 14:56:32 +1100

    Work in progress

--- outputs.tf
+++ outputs.tf
@@ -94,11 +94,11 @@ output "ecs_scale_down_alarm_arn" {
 }
 
 output "ecs_scale_up_sns_topic_arn" {
-  description = "ARN of CPU CloudWatch alarm that triggers scale up event"
-  value = var.ecs_task_scaling_enabled == true ? aws_sns_topic.cpu_utilization_up[0].arn : null
+  description = "ARN of CPU CloudWatch topic that triggers scale up event"
+  value = aws_sns_topic.cpu_utilization_up.arn
 }
 
 output "ecs_scale_down_sns_topic_arn" {
-  description = "ARN of CPU CloudWatch alarm that triggers scale down event"
-  value = var.ecs_task_scaling_enabled == true ? aws_sns_topic.cpu_utilization_down[0].arn : null
+  description = "ARN of CPU CloudWatch topic that triggers scale down event"
+  value = aws_sns_topic.cpu_utilization_down.arn
 }
